5c2d0ae1365ab47ce6a5a3bd5ab2af1f3c611be2
Dal-os <osdal778@gmail.com.com>
2025-12-14 11:39:34 +0800

    Fix Amazing Grace lyrics display

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function showSong(title) {
   let lyrics = "";
 
   if (title === "Amazing Grace") {
-    lyrics = "Amazing grace how sweet the sound...";
+    lyrics = "Amazing grace, how sweet the sound\nThat saved a wretch like me\nI once was lost, but now am found\nWas blind, but now I see";
   }
 
   if (title === "How Great Thou Art") {
-    lyrics = "O Lord my God, when I in awesome wonder...";
+    lyrics = "O Lord my God, when I in awesome wonder\nConsider all the worlds Thy hands have made\nI see the stars, I hear the rolling thunder\nThy power throughout the universe displayed";
   }
 
   document.getElementById("lyrics").innerText = lyrics;
